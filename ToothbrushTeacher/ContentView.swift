e5638f23f138d4a056ed6bb4c51e829588bfff38
o-benjamin <06enjamin@gmail.com>
2026-05-24 10:02:37 +0900

    Initial Commit

---
+++ ToothbrushTeacher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToothbrushTeacher
+//
+//  Created by Kenshin Sasaki on 2026/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToothbrushTeacher/ToothbrushTeacherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToothbrushTeacherApp.swift
+//  ToothbrushTeacher
+//
+//  Created by Kenshin Sasaki on 2026/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ToothbrushTeacherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
